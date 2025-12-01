44835935bb27c38e1db1e835c25c600a9f2e2acf
angelrdz013 <146151289+angelrdz013@users.noreply.github.com>
2025-11-30 23:18:31 -0600

    Update supabaseClient.ts

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -4,3 +4,4 @@ const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON as string;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
